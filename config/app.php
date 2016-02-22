d0f988eb8ef6cfdd01a69508b07f7e7c0a5c191e
Jacky Liang <bluelegopiece@hotmail.com>
2016-02-22 14:34:54 -0500

    task #5: installed laravelpanel and updated composer files

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'debug' => env('APP_DEBUG', false),
+    'debug' => env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -144,6 +144,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Serverfireteam\Panel\PanelServiceProvider::class,
 
     ],
 
