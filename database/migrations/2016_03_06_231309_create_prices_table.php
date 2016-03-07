a3641713e47118bf93c7e9d43dd438eed3b3dec3
Jacky Liang <bluelegopiece@hotmail.com>
2016-03-06 19:01:57 -0500

    task #9: added migration for Prices. Added crud panel skeletons for Prices

---
+++ database/migrations/2016_03_06_231309_create_prices_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePricesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Prices', function (Blueprint $table) {
+            //
+            $table->increments('id');
+            $table->date('date');
+            $table->integer('symbol_id')->unsigned(); // Prices.symbol_id references Symbols.id
+            $table->float('open')->nullable();
+            $table->float('high')->nullable();
+            $table->float('low')->nullable();
+            $table->float('last')->nullable();
+            $table->float('settle')->nullable();
+            $table->integer('volume')->nullable();
+            $table->timestamps();
+
+            // Foreign key constraint
+            $table->foreign('symbol_id')
+                ->references('id')->on('Symbols')
+                ->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('Prices', function (Blueprint $table) {
+            Schema::drop('Prices');
+        });
+    }
+}
