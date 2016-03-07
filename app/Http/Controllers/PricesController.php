9fa8aacfec5f0ee20acf1b5c6ec23843d68c6bd3
Jacky Liang <bluelegopiece@hotmail.com>
2016-03-06 19:26:11 -0500

    close #9: added panels for Prices

--- app/Http/Controllers/PricesController.php
+++ app/Http/Controllers/PricesController.php
@@ -13,22 +13,27 @@ class PricesController extends CrudController{
     public function all($entity){
         parent::all($entity); 
 
-        /** Simple code of  filter and grid part , List of all fields here : http://laravelpanel.com/docs/master/crud-fields
 
+		$this->filter = \DataFilter::source(new \App\Prices);
+		$this->filter->add('prices', 'Prices', 'text');
+		$this->filter->submit('search');
+		$this->filter->reset('reset');
+		$this->filter->build();
 
-			$this->filter = \DataFilter::source(new \App\Category);
-			$this->filter->add('name', 'Name', 'text');
-			$this->filter->submit('search');
-			$this->filter->reset('reset');
-			$this->filter->build();
+		$this->grid = \DataGrid::source($this->filter);
+        $this->grid->add('id', 'ID', true);
+        $this->grid->add('date', 'Date', true);
+        $this->grid->add('symbol_id', 'Symbol ID', true);
+        $this->grid->add('open', 'Open', true);
+        $this->grid->add('high', 'High', true);
+        $this->grid->add('low', 'Low', true);
+        $this->grid->add('last', 'Last', true);
+        $this->grid->add('settle', 'Settle', true);
+        $this->grid->add('volume', 'Volume', true);
+        $this->grid->add('created_at', 'Created At', true);
+        $this->grid->add('updated_at', 'Updated At', true);
+		$this->addStylesToGrid();
 
-			$this->grid = \DataGrid::source($this->filter);
-			$this->grid->add('name', 'Name');
-			$this->grid->add('code', 'Code');
-			$this->addStylesToGrid();
-
-        */
-                 
         return $this->returnView();
     }
     
@@ -36,19 +41,17 @@ class PricesController extends CrudController{
         
         parent::edit($entity);
 
-        /* Simple code of  edit part , List of all fields here : http://laravelpanel.com/docs/master/crud-fields
-	
-			$this->edit = \DataEdit::source(new \App\Category());
+		$this->edit = \DataEdit::source(new \App\Prices());
+		$this->edit->label('Edit Prices');
+        $this->edit->add('date', 'Date i.e. 2010-05-13', 'text')->rule('required|date_format:Y-m-d');
+        $this->edit->add('symbol_id', 'Symbol ID', 'text')->rule('required');
+        $this->edit->add('open', 'Open', 'text')->rule('required');
+        $this->edit->add('high', 'High', 'text')->rule('required');
+        $this->edit->add('low', 'Low', 'text')->rule('required');
+        $this->edit->add('last', 'Last', 'text')->rule('required');
+        $this->edit->add('settle', 'Settle', 'text')->rule('required');
+        $this->edit->add('volume', 'Volume', 'text')->rule('required');
 
-			$this->edit->label('Edit Category');
-
-			$this->edit->add('name', 'Name', 'text');
-		
-			$this->edit->add('code', 'Code', 'text')->rule('required');
-
-
-        */
-       
         return $this->returnEditView();
     }    
 }
