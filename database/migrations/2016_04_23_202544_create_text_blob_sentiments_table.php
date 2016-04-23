6f8688d87946b40ed031ff9defd4e6e1116e5f9b
Jacky Liang <bluelegopiece@hotmail.com>
2016-04-23 16:29:14 -0400

    task #21: added four sentiment engine tables

---
+++ database/migrations/2016_04_23_202544_create_text_blob_sentiments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTextBlobSentimentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('TextBlobSentiments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->date('date')->unique();
+            $table->integer('symbol_id')->unsigned(); // TextBlobSentiments.symbol_id references Symbols.id
+            $table->integer('polarity');
+            $table->timestamps();
+
+            $table->foreign('symbol_id')
+                ->references('id')->on('Symbols')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('TextBlobSentiments');
+    }
+}
