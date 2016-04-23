8f2fb4016dfec8a33ae98080b73a944dadeeee4d
Jacky Liang <bluelegopiece@hotmail.com>
2016-04-23 16:49:05 -0400

    task #2: added opinionfinder controllers and model, and updated stanford nlp

--- app/Http/Controllers/OpinionFinderSentimentsController.php
+++ app/Http/Controllers/OpinionFinderSentimentsController.php
@@ -8,27 +8,24 @@ use \Serverfireteam\Panel\CrudController;
 
 use Illuminate\Http\Request;
 
-class StanfordNLPSentimentsController extends CrudController{
+class OpinionFinderSentimentsController extends CrudController{
 
     public function all($entity){
-        parent::all($entity); 
+        parent::all($entity);
 
+        $this->filter = \DataFilter::source(new \App\OpinionFinderSentiments);
+        $this->filter->add('opinionfinder', 'OpinionFinder', 'text');
+        $this->filter->submit('search');
+        $this->filter->reset('reset');
+        $this->filter->build();
 
-
-		$this->filter = \DataFilter::source(new \App\StanfordNLPSentiments);
-		$this->filter->add('stanfordnlp', 'StanfordNLP', 'text');
-		$this->filter->submit('search');
-		$this->filter->reset('reset');
-		$this->filter->build();
-
-		$this->grid = \DataGrid::source($this->filter);
+        $this->grid = \DataGrid::source($this->filter);
         $this->grid->add('id', 'ID', true);
         $this->grid->add('date', 'Date', true);
         $this->grid->add('symbol_id', 'Symbol ID', true);
         $this->grid->add('polarity', 'Polarity', true);
         $this->grid->add('created_at', 'Created At', true);
         $this->grid->add('updated_at', 'Updated At', true);
-		$this->addStylesToGrid();
 
         return $this->returnView();
     }
@@ -37,13 +34,12 @@ class StanfordNLPSentimentsController extends CrudController{
         
         parent::edit($entity);
 
+        $this->edit = \DataEdit::source(new \App\OpinionFinderSentiments());
 
-		$this->edit = \DataEdit::source(new \App\StanfordNLPSentiments());
+        $this->edit->label('Edit Category');
 
-		$this->edit->label('Edit Category');
-
-		$this->edit->add('date', 'Date', 'text')->rule('required');
-		$this->edit->add('symbol_id', 'Symbol ID', 'text')->rule('required');
+        $this->edit->add('date', 'Date', 'text')->rule('required');
+        $this->edit->add('symbol_id', 'Symbol ID', 'text')->rule('required');
         $this->edit->add('polarity', 'Polarity', 'text')->rule('required');
 
         return $this->returnEditView();
